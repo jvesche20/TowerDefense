fe9d016c6e59e64404a85230c350694968d55379
jvesche20 <70725626+jvesche20@users.noreply.github.com>
2021-03-08 20:17:17 -0500

    wave v1

---
+++ Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+
+
+    public float timeWaves = 5f;
+    private float countDown = 2f;
+
+    private int waveNumber = 1;
+
+    void Update()
+    {
+        if(countDown <= 0)
+        {
+            SpawnWave();
+            countDown = timeWaves;
+        }
+        // removes countDown once every second
+        countDown -= Time.deltaTime;
+    }
+
+    void SpawnWave()
+    {
+
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+        waveNumber++;
+
+    }
+    //numOfEnemies = waveNumber* waveNumber + 1;
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
